28e9c84f234031809bb0cd132ac0e6918da592a9
ASS <550241840@qq.com>
2021-06-26 15:02:54 +0800

    前端初步完成

--- vue/src/views/school/sch.vue
+++ vue/src/views/school/sch.vue
@@ -21,7 +21,7 @@
 
     <el-container  style="; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1', '3']",>
+        <el-menu :default-openeds="['1', '3']">
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-plus"></i>导航一</template>
             <el-menu-item index="1-1">选项1</el-menu-item>
